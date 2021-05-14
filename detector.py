923d488f535eceeaf0be0eaf39999ac7326b52eb
Fort21 <oyinladeabioye@gmail.com>
2021-05-14 13:51:02 +0100

    I modified the code

--- detector.py
+++ detector.py
@@ -1,4 +1,4 @@
-#LOAD LIBRARIES
+#IMPORT LIBRARIES
 import numpy as np
 import cv2
 import pandas as pd
@@ -7,8 +7,10 @@ import pandas as pd
 
 face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-#READ VIDEO
+#SET VIDEO
 cap = cv2.VideoCapture(0)
+
+#LOOP
 
 while 1:
     ret, frame = cap.read()
@@ -19,16 +21,18 @@ while 1:
 
     #DRAW RECTANGLES
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+y,w+h), (255, 0, 0), 5)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 5)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color=frame[y:y+h, x:x+w]
 
+
+# SHOW IMAGE
 
     cv2.imshow("image", frame)
 
 
 
-    cv2.waitKey(0) & 0xff
+    k = cv2.waitKey(5) & 0xff
     if k=="q":
         break
 
